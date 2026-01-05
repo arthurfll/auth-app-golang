9ade9271cb41a49e1c42712fd5a5ced0a27c7d15
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2026-01-05 09:25:06 -0300

    get all function

---
+++ GetAllNotesFunction.cs
@@ -0,0 +1,40 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Identity;
+using Azure.Data.Tables;
+
+namespace azure_function_app;
+
+public class GetAllNotesFunction
+{
+    private readonly ILogger<GetAllNotesFunction> _logger;
+
+    public GetAllNotesFunction(ILogger<GetAllNotesFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GetAllNotesFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    {
+        DefaultAzureCredential credential = new();
+        TableServiceClient serviceClient = new(
+            endpoint: new Uri("https://arthurfll8059.table.core.windows.net/"),
+            credential
+        );
+
+        TableClient client = serviceClient.GetTableClient(
+            tableName: "notes"
+        );
+
+        var entities = client.Query<TableEntity>().ToList();
+
+
+
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(entities);
+    }
+}
+

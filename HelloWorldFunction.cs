0699f76358755d056c52056b8a607c84270a832a
João Arthur Gonçalves Fontenelle <arthurfontenelle2017@gmail.com>
2026-01-05 08:47:01 -0300

    .

---
+++ HelloWorldFunction.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace azure_function_app;
+
+public class HelloWorldFunction
+{
+    private readonly ILogger<HelloWorldFunction> _logger;
+
+    public HelloWorldFunction(ILogger<HelloWorldFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HelloWorldFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
